dc4d39ff1f546fab7c409686d440286edf20596a
hseffian <hassanseffiani@gmail.com>
2019-12-19 21:58:05 +0100

    0

---
+++ test.html/test.js
@@ -0,0 +1,9 @@
+function My_function()
+{
+    document.getElementById('red').style.backgroundColor='red';
+}
+
+function My_function1()
+{
+    document.getElementById('red').style.backgroundColor='white';
+}

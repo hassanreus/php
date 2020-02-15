605304037bfdd1e5771b039c18b8217be018bea4
hseffian <hassanseffiani@gmail.com>
2020-02-15 10:00:25 +0100

    0

--- day02/ex01/one_more_time.php
+++ day02/ex01/one_more_time.php
@@ -14,9 +14,11 @@
                 if (preg_match("/\b((l|L)undi|(m|M)ardi|(m|M)ercredi|(J|j)eudi|(V|v)endredi|(S|s)amedi|(D|d)imanche)\b/",$string[0]) && preg_match("/^(0(0)?[1-9]?|[1-9]|[12]\d|3[0-1])$/", $string[1]) && preg_match("/\b(([j|J]anvier|[F|f]évrier|[M-m]ars|[A-a]vril|[M-m]ai|[J-j]uin|[J-j]uillet|[A-a]oût|[S-s]eptembre|[O-o]ctobre|[N-n]ovembre|[D-d]écembre))\b/", $string[2]) && preg_match("/^\d{4}$/", $string[3]) && preg_match("/^(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])$/", $string[4])){
                     $time = explode(':', $string[4]);
                     date_default_timezone_set('Europe/Paris');
-                    echo mktime($time[0], $time[1], $time[2], is_month($string[2]), $string[1], $string[3]);
-                }
+					echo mktime($time[0], $time[1], $time[2], is_month($string[2]), $string[1], $string[3])."\n";
+					exit ;
+				}
             }
-        }
+		}
+		echo "Wrong Format\n";
     }
 ?>
--- day02/ex04/photos.php
+++ day02/ex04/photos.php
@@ -1,4 +1,27 @@
 #!/usr/bin/php
 <?php
-    
+	if ($argc == 2){
+		$string =  $argv[1];
+		$handle = curl_init();
+		curl_setopt($handle, CURLOPT_URL, $string);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+		$data = curl_exec($handle);
+		// $string = preg_replace("http:\/\/.*", $string = substr($string, 7),$string);
+		if (preg_match("/^http:\/-|(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/", $string, $matchs) && $data){
+			//echo $matchs[0];
+			$path = $matchs[0];
+			$file = $path;
+			if (!file_exists($path)){
+				mkdir($path);
+				echo $data;
+				if (preg_match('/<img[^>]+src\s*=\s*"([^">]+)/', $data, $matchs)){
+					echo $matchs[1];
+				}
+				//$img = file_get_contents("http://".$path."images/home_big.jpg",NULL);
+				//file_put_contents($file."omg.jpg" ,$img);
+			}
+		}else
+			echo "EROOR\n";
+		curl_close($handle);
+	}
 ?>
